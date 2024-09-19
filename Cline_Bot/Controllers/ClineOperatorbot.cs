d6f4eb9edf2dd062eefd4e9b71785720e7cebe48
sardorbek002 <sardorbekibroximov0602@gmail.com>
2024-09-19 16:56:09 +0500

    Add project files.

---
+++ Cline_Bot/Controllers/ClineOperatorbot.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace FIrstBotASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        // TelegramBotClient sinfidan obyekt hosil qilamiz
+        // va unga Botfather orqali olgan tokenni
+        // bog'laymiz
+        private TelegramBotClient client = new TelegramBotClient("1531755050:AAEpi-2sgSeqoP8E_DVEe0Csx0H8L1zBi2c");
+
+        // HomePage
+        public string Index()
+        {
+            // yangi event_handler yasaldi
+            client.OnMessage += Xabar_Kelganda;
+
+            // xabar kelishini tasdiqlash
+            client.StartReceiving();
+            
+            // string qaytaradi  
+            return "Bot hozr ishlamoqda";
+        }
+
+        // foydalanuvchu xabar yuborganda ishlaydi
+        private async void Xabar_Kelganda(object sender, MessageEventArgs e)
+        {
+            // foydalanuvchi idsi
+            long userId = e.Message.Chat.Id;
+            
+            // kelgan xabar idsi
+            int msgId = e.Message.MessageId;
+
+            if(e.Message.Text == "/start")
+            {
+                // xabar yuborish
+                await client.SendTextMessageAsync(userId, "Assalomu alaykum", replyToMessageId: msgId);
+            }
+        }
+    }
+}
---
+++ Cline_Bot/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cline_Bot.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
